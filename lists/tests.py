a2529734fb2b7892bea94f67eef6a8ff88559a10
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-04-11 14:28:45 -0500

    refactoring functional test

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -17,4 +18,22 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		# Set up test
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		
+		# Exercise: call the function under test
+		response = home_page(request)
+
+		# Assert
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string(
+			'home.html',
+			{'new_item_text': 'A new list item'}
+		)
+		self.assertEqual(response.content.decode(), expected_html)
+		
 
