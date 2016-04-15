cdf99e02baa200c1d9786c016f6306e35f47b382
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-04-15 17:07:07 -0500

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -29,14 +29,24 @@ class HomePageTest(TestCase):
 		# Exercise: call the function under test
 		response = home_page(request)
 
-		# Assert
-		self.assertIn('A new list item', response.content.decode())
-		expected_html = render_to_string(
-			'home.html',
-			{'new_item_text': 'A new list item'}
-		)
-		self.assertEqual(response.content.decode(), expected_html)
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
 
+	def test_home_page_redirects_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+	def test_home_page_only_saves_items_when_necessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
@@ -56,3 +66,19 @@ class ItemModelTest(TestCase):
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
+		
+	def test_displays_all_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+
+		self.assertContains(response, 'itemey 1')
+		self.assertContains(response, 'itemey 2')	
+
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 def home_page(request):
-	return render(request, 'home.html', {
-		'new_item_text': request.POST.get('item_text', ''),
-	})
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/lists/the-only-list-in-the-world/')
+	return render(request, 'home.html')
 
+def view_list(request):
+	items = Item.objects.all()
+	return render(request, 'list.html', {'items': items})
